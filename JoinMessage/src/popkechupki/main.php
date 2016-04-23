1a2635a248dc81a088353edb491c4c62c19d0560
popkechupki <hobbypopkechupki@gmail.com>
2016-04-23 14:42:29 +0900

    Added files via upload

---
+++ JoinMessage/src/popkechupki/main.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Sunshine;
+
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\utils\TextFormat;
+use pocketmine\utils\Config;
+
+class main extends PluginBase implements Listener{ 
+
+	public function onEnable(){
+        $plugin = "JoinMessage";                                        
+        $this->getLogger()->info(TextFormat::GREEN.$plugin."を読み込みました".TextFormat::BLUE." By Sunshine");
+        $this->getLogger()->info(TextFormat::RED.$plugin."を二次配布するのは禁止です");                
+        $this->getServer()->getPluginManager()->registerEvents($this,$this);
+
+	if (!file_exists($this->getDataFolder())) @mkdir($this->getDataFolder(), 0740, true);
+		$this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML,
+		array(
+			'一行目' => '〇〇サーバーへようこそ！'
+            ));
+        $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML,
+        array(
+			'二行目' => 'このサーバーでは〇〇ができます！'
+            ));
+        $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML,
+        array(
+			'三行目' => 'どうぞお楽しみください！'
+			));
+	
+        $this->config->save();
+
+    }
+
+    public function onPlayerJoin(PlayerJoinEvent $event) {
+    	$player = $event->getPlayer();
+        $date1 = $this->config->get("一行目");
+        $date2 = $this->config->get("二行目");
+        $date3 = $this->config->get("三行目");
+    	$player->sendMessage("§b+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+§r");
+        $player->sendMessage("$date1");
+        $player->sendMessage("$date2");
+        $player->sendMessage("$date3");
+    	$player->sendMessage("§b+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+§r");
+        $Name = $player->getName();
+        $event->setJoinMessage("");
+        $this->getServer()->broadcastPopup($Name."§b様がログインしました！");
+    }
+}
+
